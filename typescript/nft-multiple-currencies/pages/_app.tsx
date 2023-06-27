3268e95bc71c4309a0d8451ce6a9bde803b6b601
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-06-26 22:27:59 -1000

    Update _app.tsx

--- typescript/nft-multiple-currencies/pages/_app.tsx
+++ typescript/nft-multiple-currencies/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mumbai;
+const activeChainId = ChainId.Polygon;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
